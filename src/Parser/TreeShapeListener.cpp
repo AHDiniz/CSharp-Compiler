bc39185f88c22a68420a69a637394fcab125d53a
Alan H. Diniz <alanhdiniz@gmail.com>
2021-03-13 14:15:39 -0300

    Reorganizing again...

--- src/Parser/TreeShapeListener.cpp
+++
@@ -1,8 +0,0 @@
-#include "antlr4-runtime.h"
-#include "CSharpBaseListener.h"
-#include "TreeShapeListener.h"
-
-void TreeShapeListener::enterKey(ParserRuleContext *ctx)
-{
-    // Do something when entering the key rule
-}
