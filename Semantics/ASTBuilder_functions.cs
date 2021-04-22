d02be60ef9487c337707a6ed57edd5ae7154b3c7
Alan H. Diniz <alanhdiniz@gmail.com>
2021-04-22 15:10:17 -0300

    Fixing repeated enumerations in Node.Kind

--- Semantics/ASTBuilder_functions.cs
+++ Semantics/ASTBuilder_functions.cs
@@ -8,76 +8,14 @@ namespace CSharp_Compiler.Semantics
 {
     public partial class ASTBuilder : CSharpParserBaseListener
     {
-        public override void EnterLambda_expression(CSharpParser.Lambda_expressionContext context)
+        public override void EnterMethod_declaration(CSharpParser.Method_declarationContext context)
         {
-            
-            Console.WriteLine("Entering lambda_expression context.");
+            Console.WriteLine("Entering method_declaration context.");
         }
 
-        public override void ExitLambda_expression(CSharpParser.Lambda_expressionContext context)
+        public override void ExitMethod_declaration(CSharpParser.Method_declarationContext context)
         {
-            
-            Console.WriteLine("Exiting lambda_expression context.");
-        }
-
-        public override void EnterAnonymous_function_signature(CSharpParser.Anonymous_function_signatureContext context)
-        {
-            
-            Console.WriteLine("Entering anonymous_function_signature context.");
-        }
-
-        public override void ExitAnonymous_function_signature(CSharpParser.Anonymous_function_signatureContext context)
-        {
-            
-            Console.WriteLine("Exiting anonymous_function_signature context.");
-        }
-
-        public override void EnterExplicit_anonymous_function_parameter_list(CSharpParser.Explicit_anonymous_function_parameter_listContext context)
-        {
-            
-            Console.WriteLine("Entering explicit_anonymous_function_parameter_list context.");
-        }
-
-        public override void ExitExplicit_anonymous_function_parameter_list(CSharpParser.Explicit_anonymous_function_parameter_listContext context)
-        {
-            
-            Console.WriteLine("Exiting explicit_anonymous_function_parameter_list context.");
-        }
-
-        public override void EnterExplicit_anonymous_function_parameter(CSharpParser.Explicit_anonymous_function_parameterContext context)
-        {
-            
-            Console.WriteLine("Entering explicit_anonymous_function_parameter context.");
-        }
-
-        public override void ExitExplicit_anonymous_function_parameter(CSharpParser.Explicit_anonymous_function_parameterContext context)
-        {
-            
-            Console.WriteLine("Exiting explicit_anonymous_function_parameter context.");
-        }
-
-        public override void EnterImplicit_anonymous_function_parameter_list(CSharpParser.Implicit_anonymous_function_parameter_listContext context)
-        {
-            
-            Console.WriteLine("Entering implicit_anonymous_function_parameter_list context.");
-        }
-
-        public override void ExitImplicit_anonymous_function_parameter_list(CSharpParser.Implicit_anonymous_function_parameter_listContext context)
-        {
-            
-            Console.WriteLine("Exiting implicit_anonymous_function_parameter_list context.");
-        }
-
-        public override void EnterAnonymous_function_body(CSharpParser.Anonymous_function_bodyContext context)
-        {
-            
-            Console.WriteLine("Entering anonymous_function_body context.");
-        }
-
-        public override void ExitAnonymous_function_body(CSharpParser.Anonymous_function_bodyContext context)
-        {
-            
-            Console.WriteLine("Exiting anonymous_function_body context.");
+            Console.WriteLine("Exiting method_declaration context.");
         }
     }
 }
