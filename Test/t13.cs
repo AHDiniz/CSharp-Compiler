3861f82954feaf0cc39ca874918f006669d2601b
Fernando Machado <rodrigues.guerra@hotmail.com>
2021-04-22 14:59:00 -0300

    exemplo 13

---
+++ Test/t13.cs
@@ -0,0 +1,17 @@
+// Hello, world program
+// This program writes "hello, world" to the console
+//
+class Hello // any name will do for this class
+{
+
+    static void test(int a, int b){
+        return;
+    }
+    static void Main() { // this method must be named "Main"
+        int a;
+        int b;
+        
+        test(a, b);
+        System.Console.WriteLine("hello, world");
+    }
+}
