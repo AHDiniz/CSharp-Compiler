dfe800b16570bdcf4fc321f2fe7a906a4d50415a
Alan H. Diniz <alanhdiniz@gmail.com>
2021-04-22 15:06:03 -0300

    Adding method declaration node kind in the enumeration

--- Semantics/Node.cs
+++ Semantics/Node.cs
@@ -14,6 +14,7 @@ namespace CSharp_Compiler.Semantics
             EqualityComparision,
             MemberVariableDeclaration,
             MethodVariableDeclaration,
+            MethodDeclaration,
             ClassDefinition,
             EnumDefinition,
             MainScope,
